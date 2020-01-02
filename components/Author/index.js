6c2f5db26bbfd68d8105f71b5d281e45c0bfeaa9
Nick930826 <xianyou1993@qq.com>
2020-01-02 22:38:43 +0800

    update

--- components/Author/index.js
+++ components/Author/index.js
@@ -6,7 +6,7 @@ import styles from './style.less'
 const Author =()=>{
     return (
         <div className={cx(styles.authorDiv, styles.commBox)}>
-            <div> <Avatar size={100} src='http://s.weituibao.com/1573017829206/Nick.jpg'  /></div>
+            <div> <Avatar size={100} src=''  /></div>
             <div className={styles.authorIntroduction}>
                 大胡子程序员，专注于WEB和移动前端开发
                 <Divider>社交账号</Divider>
