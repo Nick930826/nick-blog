9bcc2d717a04697c769d379fcd90f48307b9c5ba
Nick930826 <xianyou1993@qq.com>
2020-01-02 16:09:34 +0800

    update

--- components/index.js
+++ components/index.js
@@ -2,12 +2,12 @@ import Header from './Header'
 import Author from './Author'
 import Advert from './Advert'
 import Footer from './Footer'
-import Tocify from './tocify.tsx'
+// import Tocify from './tocify.tsx'
 
 export {
   Header,
   Author,
   Advert,
   Footer,
-  Tocify
+  // Tocify
 }
