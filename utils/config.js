1a7692a99dab090f1af052790a8c21068d041dd0
Nick930826 <xianyou1993@qq.com>
2020-01-02 18:11:48 +0800

    update

--- utils/config.js
+++ utils/config.js
@@ -37,7 +37,7 @@ if (evn.isDev) {
   assetPrefix = '//s.weituibao.com/beta/Ariel'
 } else if (evn.isOnline) {
   pageDomain = 'http://blog.chennick.wang'
-  requestBaseURL = '//http://47.99.134.126:7001'
+  requestBaseURL = '//47.99.134.126:7001'
   assetPrefix = '//s.weituibao.com/release/Ariel'
 }
 
