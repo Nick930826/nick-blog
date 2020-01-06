4d0b9b4a71e4581715db34ea70405368ba776e40
Nick930826 <xianyou1993@qq.com>
2020-01-06 11:24:08 +0800

    update

---
+++ public/baidutongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?f4691efcdff0f20d244935c52e807b06";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
