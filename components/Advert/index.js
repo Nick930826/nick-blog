d505ca74fd2a66f8f1e123961938a12d08cbb022
Nick930826 <xianyou1993@qq.com>
2020-01-10 09:52:17 +0800

    添加微信群二维码

--- components/Advert/index.js
+++ components/Advert/index.js
@@ -5,9 +5,9 @@ import styles from './style.less'
 const Advert = ()=>{
   return (
     <div className={cx(styles.adDiv, styles.commBox)}>
-      <div><img src="//s.weituibao.com/1578063165814/timg.jpeg" width="100%" /></div>
-      <div><img src="//s.weituibao.com/1578063165814/timg.jpeg" width="100%" /></div>
-      <div><img src="//s.weituibao.com/1578063165814/timg.jpeg" width="100%" /></div>
+      <div><img src="//s.weituibao.com/1578621067554/WeChat55a73177f75fc52bf56028c7ac90b115.png" width="100%" /></div>
+      {/* <div><img src="//s.weituibao.com/1578063165814/timg.jpeg" width="100%" /></div>
+      <div><img src="//s.weituibao.com/1578063165814/timg.jpeg" width="100%" /></div> */}
     </div>
   )
  }
