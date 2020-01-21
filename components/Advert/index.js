abcdd792fbfa4144b5144fcb5cfcfc9b82286b65
Nick930826 <xianyou1993@qq.com>
2020-01-21 10:30:15 +0800

    更新二维码

--- components/Advert/index.js
+++ components/Advert/index.js
@@ -5,7 +5,7 @@ import styles from './style.less'
 const Advert = ()=>{
   return (
     <div className={cx(styles.adDiv, styles.commBox)}>
-      <div><img src="//s.weituibao.com/1578621067554/WeChat55a73177f75fc52bf56028c7ac90b115.png" width="100%" /></div>
+      <div><img src="//s.weituibao.com/1579573755114/WeChatb58d100bfc0f6be51bbe8a9a1d30af9a.png" width="100%" /></div>
       {/* <div><img src="//s.weituibao.com/1578063165814/timg.jpeg" width="100%" /></div>
       <div><img src="//s.weituibao.com/1578063165814/timg.jpeg" width="100%" /></div> */}
     </div>
