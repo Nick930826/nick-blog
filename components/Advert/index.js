51b0baab2ea82f7f0706a3dd2cd8c28b15c6251f
Nick930826 <xianyou1993@qq.com>
2020-02-20 10:41:00 +0800

    更新qq群图片

--- components/Advert/index.js
+++ components/Advert/index.js
@@ -5,7 +5,7 @@ import styles from './style.less'
 const Advert = ()=>{
   return (
     <div className={cx(styles.adDiv, styles.commBox)}>
-      <div><img src="//s.weituibao.com/1579573755114/WeChatb58d100bfc0f6be51bbe8a9a1d30af9a.png" width="100%" /></div>
+      <div><img src="//s.weituibao.com/1582166370443/qqchat.jpeg" width="100%" /></div>
       {/* <div><img src="//s.weituibao.com/1578063165814/timg.jpeg" width="100%" /></div>
       <div><img src="//s.weituibao.com/1578063165814/timg.jpeg" width="100%" /></div> */}
     </div>
