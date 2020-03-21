de9d16253541d5c5b91a507f1d5ebb82cb8a03ff
Nick930826 <xianyou1993@qq.com>
2020-03-21 11:24:45 +0800

    update

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,7 +3,7 @@ import styles from './style.less'
 const Footer = ()=>(
     <div className={styles.footerDiv}>
         <div>系统由 Node + React + Ant Desgin 驱动 </div>
-        <div><a href="http://beian.miit.gov.cn/">浙ICP备19047249号-1</a> 邮箱地址：xianyou1993@qq.com</div>
+        <div><a target="_blank" href="http://beian.miit.gov.cn/">浙ICP备19047249号-1</a> 邮箱地址：xianyou1993@qq.com</div>
     </div>
 )
 
