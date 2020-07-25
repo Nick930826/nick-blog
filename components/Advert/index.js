31b0820dfb70aad014fec1a6891e8976475f0b7a
Nick930826 <xianyou1993@qq.com>
2020-07-25 16:10:32 +0800

    update

--- components/Advert/index.js
+++ components/Advert/index.js
@@ -5,7 +5,8 @@ import styles from './style.less'
 const Advert = ()=>{
   return (
     <div className={cx(styles.adDiv, styles.commBox)}>
-      <div><img src="//s.weituibao.com/1582166370443/qqchat.jpeg" width="100%" /></div>
+      <h2>如果文章对你有帮助，可以给作者一点鼓励。这将是作者持续输出的动力.</h2>
+      <div><img src="//s.weituibao.com/1595662844463/2421595662835_.pic.jpg" width="100%" /></div>
       {/* <div><img src="//s.weituibao.com/1578063165814/timg.jpeg" width="100%" /></div>
       <div><img src="//s.weituibao.com/1578063165814/timg.jpeg" width="100%" /></div> */}
     </div>
